ad54acab080ee9fb6432d0d2a16ffc1b3ae302cb
Darren Cooper-Matila <dcoopermatila@gmail.com>
2022-11-29 18:13:54 +1300

    first push

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 // ./src/App.js
 
-import React, { useState } from 'react';
-import './App.css';
-import { computerVision, isConfigured as ComputerVisionIsConfigured } from './azure-cognitiveservices-computervision';
+import React, { useState } from "react";
+import "./App.css";
+import {
+  computerVision,
+  isConfigured as ComputerVisionIsConfigured,
+} from "./azure-cognitiveservices-computervision";
 
 function App() {
-
   const [fileSelected, setFileSelected] = useState(null);
   const [analysis, setAnalysis] = useState(null);
   const [processing, setProcessing] = useState(false);
-  
-  const handleChange = (e) => {
-    setFileSelected(e.target.value)
-  }
-  const onFileUrlEntered = (e) => {
 
+  const handleChange = (e) => {
+    setFileSelected(e.target.value);
+  };
+  const onFileUrlEntered = (e) => {
     // hold UI
     setProcessing(true);
     setAnalysis(null);
@@ -25,50 +26,75 @@ function App() {
       setFileSelected("");
       setProcessing(false);
     });
-
   };
 
   // Display JSON data in readable format
   const PrettyPrintJson = (data) => {
-    return (<div><pre>{JSON.stringify(data, null, 2)}</pre></div>);
-  }
+    console.log(data);
+    return (
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    );
+  };
 
   const DisplayResults = () => {
     return (
       <div>
         <h2>Computer Vision Analysis</h2>
-        <div><img src={analysis.URL} height="200" border="1" alt={(analysis.description && analysis.description.captions && analysis.description.captions[0].text ? analysis.description.captions[0].text : "can't find caption")} /></div>
+        <div>
+          <img
+            src={analysis.URL}
+            height="200"
+            border="1"
+            alt={
+              analysis.description &&
+              analysis.description.captions &&
+              analysis.description.captions[0].text
+                ? analysis.description.captions[0].text
+                : "can't find caption"
+            }
+          />
+        </div>
         {PrettyPrintJson(analysis)}
       </div>
-    )
+    );
   };
-  
+
   const Analyze = () => {
     return (
-    <div>
-      <h1>Analyze image</h1>
-      {!processing &&
-        <div>
+      <div>
+        <h1>Analyze image</h1>
+        {!processing && (
           <div>
-            <label>URL</label>
-            <input type="text" placeholder="Enter URL or leave empty for random image from collection" size="50" onChange={handleChange}></input>
+            <div>
+              <label>URL</label>
+              <input
+                type="text"
+                placeholder="Enter URL or leave empty for random image from collection"
+                size="50"
+                onChange={handleChange}
+              ></input>
+            </div>
+            <button onClick={onFileUrlEntered}>Analyze</button>
           </div>
-          <button onClick={onFileUrlEntered}>Analyze</button>
-        </div>
-      }
-      {processing && <div>Processing</div>}
-      <hr />
-      {analysis && DisplayResults()}
+        )}
+        {processing && <div>Processing</div>}
+        <hr />
+        {analysis && DisplayResults()}
       </div>
-    )
-  }
-  
+    );
+  };
+
   const CantAnalyze = () => {
     return (
-      <div>Key and/or endpoint not configured in ./azure-cognitiveservices-computervision.js</div>
-    )
-  }
-  
+      <div>
+        Key and/or endpoint not configured in
+        ./azure-cognitiveservices-computervision.js
+      </div>
+    );
+  };
+
   function Render() {
     const ready = ComputerVisionIsConfigured();
     if (ready) {
@@ -77,12 +103,7 @@ function App() {
     return <CantAnalyze />;
   }
 
-  return (
-    <div>
-      {Render()}
-    </div>
-    
-  );
+  return <div>{Render()}</div>;
 }
 
 export default App;
